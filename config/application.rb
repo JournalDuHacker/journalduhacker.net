d3e1a425a549ba0056a01e1cdc6f1c950d850bcf
Maxence Maireaux <maxence@maireaux.fr>
2025-10-17 11:23:19 +0200

    fix: Monkey load in Application

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Lobsters
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks monkey.rb])
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/extras)
