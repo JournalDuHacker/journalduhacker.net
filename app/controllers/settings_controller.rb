e695200a8f4a4a4fa3d49dc65b4848b9d0ec1f91
Maxence Maireaux <maxence@maireaux.fr>
2025-10-17 12:06:15 +0200

    fix: Replace deprecated update_attributes with update
    Rails 6+ removed update_attributes and update_attributes! methods.
    Replaced with update and update! respectively.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -26,7 +26,7 @@ class SettingsController < ApplicationController
 
     if params[:user][:password].empty? ||
         @user.authenticate(params[:current_password].to_s)
-      if @edit_user.update_attributes(user_params)
+      if @edit_user.update(user_params)
         flash.now[:success] = t(".updatesettingsflash")
         @user = @edit_user
       end
